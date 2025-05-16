66465250d0e71e2dab1fc7ab04149e6084aed004
윤병익 <yeun0908@gmail.com>
2025-05-16 12:45:57 +0900

    cloudflare workers migration complete

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,19 +4,37 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
-import netlify from "@astrojs/netlify";
+
+import cloudflare from "@astrojs/cloudflare";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.newsniper.org/",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+
   markdown: {
     shikiConfig: {
       theme: "css-variables"
     }
   },
-  output: "hybrid",
-  adapter: netlify({
-    cacheOnDemandPages: true,
+
+  output: "server",
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+      configPath: 'wrangler.jsonc',
+      persist: true,
+    },
+    imageService: {
+      provider: "cloudflare",
+      accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
+      token: process.env.CLOUDFLARE_API_TOKEN,
+    },
   }),
+  vite: {
+    build: {
+      minify: false,
+    },
+  },
 });
---
+++ astro/src/content/config.ts
@@ -0,0 +1,32 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+
+const article = defineCollection({
+  loader: glob({pattern: "[^_]*.{md,mdx}",base: "src/content/article"}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.coerce.date(),
+    draft: z.boolean().default(true),
+    categories: z.array(z.string()).default([]),
+    keywords: z.array(z.string()).default([]),
+    series: z.string().optional()
+  }),
+});
+
+const series = defineCollection({
+  loader: glob({pattern: "{[^_]*,[^_]*/index}.{md,mdx}",base: "src/content/series"}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    draft: z.boolean().default(true),
+    statusEnabled: z.boolean().default(true)
+  }),
+});
+
+const banner = defineCollection({
+  type: "content"
+})
+
+export const collections = { article, series, banner };
