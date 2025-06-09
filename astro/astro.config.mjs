9ce95a2cfc5b84ec3d587dd4a753d1a9f299f3ca
윤병익 <yeun0908@gmail.com>
2025-06-09 12:22:13 +0900

    fix

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,30 +4,35 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
-import netlify from "@astrojs/netlify";
 
 import db from "@astrojs/db";
 import studiocms from "studiocms";
-import deno from "@deno/vite-plugin"
 import tailwindcss from "@tailwindcss/vite";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.newsniper.org/",
   integrations: [sitemap(), mdx(), pagefind(), db(), studiocms()],
+
   markdown: {
     shikiConfig: {
       theme: "css-variables"
     }
   },
+
   output: "server",
-  adapter: netlify({
-    edgeMiddleware: true,
-    cacheOnDemandPages: true,
-    excludeFiles: ["./node_modules/**"],
-    imageCDN: false
-  }),
+
   vite: {
-    plugins: [deno(), tailwindcss(),]
-  }
+    plugins: [tailwindcss(),]
+  },
+
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+    platformProxy: {
+      enabled: true,
+      configPath: "wrangler.jsonc"
+    }
+  }),
 });
